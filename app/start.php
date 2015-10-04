d1d78902e5d75af9fb6fd635a4f3d805aa463180
SanketDG <sanketdasgupta@gmail.com>
2015-10-04 23:03:54 +0530

    configure basic route for / to render a view (with a template)
    Twig is used for the templates. A route / (index) is configured to
    render home.php which renders the template from the templates dir.
    
    home.php inherits from templates/partials/default.php.

--- app/start.php
+++ app/start.php
@@ -1,6 +1,9 @@
 <?php
 
 use Slim\Slim;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
 use Noodlehaus\Config;
 
 use Myproject\User\User;
@@ -15,7 +18,9 @@ define(INC_ROOT, dirname(__DIR__));
 require INC_ROOT . '/vendor/autoload.php';
 
 $app = new Slim([
-    'mode' => file_get_contents(INC_ROOT . '/mode.php')
+    'mode' => file_get_contents(INC_ROOT . '/mode.php'),
+    'view' => new Twig(),
+    'templates.path' => INC_ROOT . '/app/views'
 ]);
 
 $app->configureMode($app->config('mode'), function() use ($app) {
@@ -23,9 +28,20 @@ $app->configureMode($app->config('mode'), function() use ($app) {
 });
 
 require 'database.php';
+require 'routes.php';
 
 $app->container->set('user', function() {
     return new User;
 });
+
+$view = $app->view;
+
+$view->parserOptions = [
+    'debug' => $app->config->get('twig.debug')
+];
+
+$view->parserExtensions = [
+    new TwigExtension
+];
 
 ?>
---
+++ app/views/templates/partials/navigation.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="{{ urlFor('home') }}">Home</a></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
